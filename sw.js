c7c6a8881c1d46cd991216f0f287560831d125ea
Gustavo Kramer <gugakramer-boop@users.noreply.github.com>
2026-05-27 09:45:40 -0400

    data: v4.7.8: fix CSS - checkbox dentro do dropdown nao herda mais flex:1/min-width:140px (filtros agora usam seletor de filho direto) (2026-05-27 09:45, 1709 lancamentos)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // TB Financeiro service worker
-const CACHE = 'tb-finance-v4.7.7';
+const CACHE = 'tb-finance-v4.7.8';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png'];
 
 self.addEventListener('install', e => {
