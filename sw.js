a47eca54f958b523862c3b278d12f2b627745b25
Gustavo Kramer <gugakramer-boop@users.noreply.github.com>
2026-05-27 12:20:52 -0400

    data: v4.9.1: Itens - row de Total no final (2026-05-27 12:20, 1709 lancamentos)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // TB Financeiro service worker
-const CACHE = 'tb-finance-v4.9.0';
+const CACHE = 'tb-finance-v4.9.1';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png'];
 
 self.addEventListener('install', e => {
