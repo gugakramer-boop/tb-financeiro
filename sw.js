703ff05b392ac93f8576e2af00e0b00f2023ed34
Gustavo Kramer <gugakramer-boop@users.noreply.github.com>
2026-05-27 10:19:06 -0400

    data: v4.8.0: tabela moderna em Itens (sort + resize de colunas, no wrap, sticky head); filtros com popup maior + scroll funcional; DRE agora agrupa por Bucket (2026-05-27 10:19, 1709 lancamentos)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // TB Financeiro service worker
-const CACHE = 'tb-finance-v4.7.9';
+const CACHE = 'tb-finance-v4.8.0';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png'];
 
 self.addEventListener('install', e => {
