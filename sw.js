7f68c332ec20d0f0b773031e3136c65adfe4f8da
Gustavo Kramer <gugakramer-boop@users.noreply.github.com>
2026-05-27 14:59:08 -0400

    data: v4.9.8: what-if agrupa por bucket; metas com exemplos inspiradores (2026-05-27 14:59, 1709 lancamentos)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // TB Financeiro service worker
-const CACHE = 'tb-finance-v4.9.7';
+const CACHE = 'tb-finance-v4.9.8';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png'];
 
 self.addEventListener('install', e => {
